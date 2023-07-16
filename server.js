7184245da998d3580f8ae47f9b4cdda83cc6a87b
Disha <bidisha2125@gmail.com>
2023-07-16 13:06:06 +0530

    Created user controller an routes

--- server.js
+++ server.js
@@ -26,9 +26,12 @@ app.use(express.json());
 
 app.use(cookieParser());
 
+// routes
 app.use("/", express.static(path.join(__dirname, "public")));
 
 app.use("/", require("./routes/root"));
+
+app.use('/users', require("./routes/userRoutes"));
 
 app.all("*", (req, res) => {
   res.status(404);
